7d5fb86dbe6867c56ff0e74d711b23d5685d1bd9
khatrisusan <khatrisusan>
2019-09-16 10:25:26 +0200

    added ID dynamically and made nav responsive

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ function showList(data) {
     const template = document.querySelector("template").content;
     const myCopy = template.cloneNode("true");
     const section = document.createElement("section");
-    section.setAttribute("id", data);
+    section.setAttribute("id", data.gsx$categorymeal.$t);
+     document.querySelector("main").appendChild(section);
     myCopy.querySelector("h1").textContent = data.gsx$dishtitle.$t;
     myCopy.querySelector("article.image img").src = "image/" + data.gsx$imagename.$t;
     document.querySelector("main").appendChild(myCopy);
