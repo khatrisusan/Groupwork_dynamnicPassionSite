e86ab3f7bced2d493dee9b42424b304d0043d23f
Mégane <megane.londono@hotmail.com>
2019-09-17 09:16:14 +0200

    last commit on branch

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-const spreadsheetID =
-    "11uCM4X1aMt_7X0s6qUkZyXIoopkTqrTyISDarVU6kQ0";
-const endpoint = `https://spreadsheets.google.com/feeds/list/${spreadsheetID}/od6/public/values?alt=json`
+const spreadsheetID = "11uCM4X1aMt_7X0s6qUkZyXIoopkTqrTyISDarVU6kQ0";
+const endpoint = `https://spreadsheets.google.com/feeds/list/${spreadsheetID}/od6/public/values?alt=json`;
 
-fetch(endpoint).then(res => res.json()).then(getData);
+fetch(endpoint)
+    .then(res => res.json())
+    .then(getData);
 
 function getData(data) {
     const myArray = data.feed.entry;
-    myArray.forEach(showList)
+    myArray.forEach(showList);
 }
 
 function showList(data) {
     console.log(data);
-    const template = document.querySelector("template").content;
-    const myCopy = template.cloneNode("true");
+    const indexTemplate = document.querySelector(".indexTemplate").content;
+    const myCopy = indexTemplate.cloneNode("true");
     const section = document.createElement("section");
     section.setAttribute("id", data);
     myCopy.querySelector("h1").textContent = data.gsx$dishtitle.$t;
-    myCopy.querySelector("article.image img").src = "image/" + data.gsx$imagename.$t;
+    myCopy.querySelector("article.image img").src =
+        "image/" + data.gsx$imagename.$t;
     document.querySelector("main").appendChild(myCopy);
+
+    const breakfastTemplate = document.querySelector(
+        "template.breakfastTemplate"
+    ).content;
+    const mySecondCopy = breakfastTemplate.cloneNode("true");
+    mySecondCopy.querySelector("p.dish_description").textContent =
+        data.gsx$dishdescription.$t;
+    document.querySelector("main").appendChild(mySecondCopy);
 }
-
-
-
-
-
-
