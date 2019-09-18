5836c55bd290aa9bab196f1f7ce5fb3bb154f239
khatrisusan <khatrisusan>
2019-09-18 15:08:42 +0200

    dynamically added all contents

--- main2.js
+++ main2.js
@@ -25,39 +25,30 @@ function rawData(data) {
 function showSheet(data) {
     console.log(data);
     const clone = document.querySelector("template.two").content;
-        const cloneData = clone.cloneNode("true");
+    const cloneData = clone.cloneNode("true");
 
-    if(data.gsx$dishtitle.$t == id) {
+    if (data.gsx$dishtitle.$t == id) {
         //console.log("yes");
-    cloneData.querySelector("p.description1").textContent = data.gsx$outfitdescription1.$t;
-   cloneData.querySelector("p.description2").textContent = data.gsx$outfitdescription2.$t;
-    cloneData.querySelector("p.description3").textContent = data.gsx$outfitdescription3.$t;
-    cloneData.querySelector("img.img1").src = "image/" + data.gsx$fashionimage1.$t;
+        cloneData.querySelector("h1.dish-title").textContent = data.gsx$dishtitle.$t;
+        cloneData.querySelector("p.recipe").textContent = data.gsx$recipe.$t;
+        cloneData.querySelector("p.author").textContent = data.gsx$author.$t;
+        cloneData.querySelector("p.date").textContent = data.gsx$date.$t;
+        cloneData.querySelector("p.rating1").textContent = data.gsx$rating1.$t;
+        cloneData.querySelector("p.rating2").textContent = data.gsx$rating2.$t;
+        cloneData.querySelector("p.rating3").textContent = data.gsx$rating3.$t;
+        cloneData.querySelector("p.description1").textContent = data.gsx$outfitdescription1.$t;
+        cloneData.querySelector("p.description2").textContent = data.gsx$outfitdescription2.$t;
+        cloneData.querySelector("p.description3").textContent = data.gsx$outfitdescription3.$t;
+        cloneData.querySelector("img.img1").src = "image/" + data.gsx$fashionimage1.$t;
         cloneData.querySelector("img.img2").src = "image/" + data.gsx$fashionimage2.$t;
-          cloneData.querySelector("img.img3").src = "image/" + data.gsx$fashionimage3.$t;
-    //data.gsx$outfitdescription3.$t;
-    //cloneData.querySelector("img.img2").src = "image/" + data.gsx$scrambledeggfashionimage.$t;
+        cloneData.querySelector("img.img3").src = "image/" + data.gsx$fashionimage3.$t;
+        //data.gsx$outfitdescription3.$t;
+        //cloneData.querySelector("img.img2").src = "image/" + data.gsx$scrambledeggfashionimage.$t;
 
-    //data.gsx$outfitdescription3.$t;
-    //cloneData.querySelector("img.img3").src = "image/" + data.gsx$scrambledeggfashionimage.$t;
-  document.querySelector("aside").appendChild(cloneData)
+        //data.gsx$outfitdescription3.$t;
+        //cloneData.querySelector("img.img3").src = "image/" + data.gsx$scrambledeggfashionimage.$t;
+        document.querySelector("aside").appendChild(cloneData)
 
-            }
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
